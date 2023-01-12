e2b881a37e058d4e2372833891f3b7a7c8202805
lucas <lucashampell@gmail.com>
2023-01-12 20:46:30 -0300

    Initial Commit

---
+++ MaryMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaryMe
+//
+//  Created by lucas meireles on 12/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
